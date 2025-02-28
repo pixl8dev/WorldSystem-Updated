5d9dbd89c51c34d855bba277bd749ca16e68804e
cycodly <114483978+cycodly@users.noreply.github.com>
2025-02-28 11:17:10 +0000

    fixes

--- src/main/java/de/butzlabben/world/GameProfileBuilder.java
+++ src/main/java/de/butzlabben/world/GameProfileBuilder.java
@@ -12,7 +12,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection; */
-import java.util.Base64;
+//https://bitbucket.org/snakeyaml/snakeyaml/commits/ee85b4f37189c3be6ac3a772171bc92ed867c7bf
+import java.util.Base64; 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
